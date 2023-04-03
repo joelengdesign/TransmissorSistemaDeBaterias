afa51f625360a679168030598e6a6d35f3a68af8
joelengdesign <joel.carvalho@itec.ufpa.br>
2023-04-03 19:03:11 -0300

    acrescentado função de mostrar as variaveis float

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 // Código de transmissão de dados da aplicação SISTEMA DE BATERIAS relacionada ao Smart Campus da UFPA
-// última atualização em 29 de março de 2023
+// código editado em 03 de abril de 2023
 
 #include <Arduino.h>
 #include <lmic.h> // biblioteca lmic para transmissão LoRa
 #include <hal/hal.h>
-#include <MODBUS.h> // biblioteca MODBUS criada para extração de dados da UACT
+#include <MODBUS.h> // biblioteca MODBUS criada para extração de dados da UACT CC
+#include <TimeLib.h> // biblioteca para converter timestamp no formato unix para hora e data
 #include <softwareReset.hpp> // biblioteca para resetar o microcontrolador
 
 MODBUS modbus(23,LED_BUILTIN);
@@ -31,7 +32,6 @@ const lmic_pinmap lmic_pins = {
   .dio = {2, 6, 7},
 };
 
-
 const byte aplicacao = 0x04; // código do sistema de baterias
 
 // parâmetros do protocolo MODBUS
@@ -40,6 +40,23 @@ const byte TypeRegisters = 0x04; // código de acesso dos registradores do tipo 
 const uint16_t InitAdress = 0x0004; // endereço do registrador inicial
 const uint16_t QuantRegisters = 0x001A; // quantidade de registradores
 
+union {
+  float valor_float;
+  uint32_t valor_inteiro;
+} conversor;
+
+float TensaoBarramento;
+float Corrente1;
+float Corrente2;
+float Corrente3;
+float Corrente4;
+float Corrente5;
+float Corrente6;
+float Corrente7;
+float Corrente8;
+float PotenciaCC;
+float EnergiaFornecida;
+float EnergiaConsumida;
 
 // parâmetros dos dados recebidos da UAC CC
 struct UACTStruct
@@ -78,22 +95,22 @@ void espera(unsigned long tempo) {
 void clearReceivedPackage(){
   for (byte i = 0; i < sizeof(uactComponent.packetReceived); i++) uactComponent.packetReceived[i] = 0x0;
   uactComponent.receivedByteIndex = 0;
-  Serial.println("limpei o pacote");
 }
 
 // função que limpa o payload de transmissão com todos os dados tratados
 void clearReceivedPackageTotal(){
   for (byte i = 0; i < sizeof(uactComponent.PkgTotal); i++) uactComponent.PkgTotal[i] = 0x0;
-  Serial.println("limpei o pacote Total");
 }
 
-// função que recebe o pacote da UACT CC
-void receivedPkgTimerCallback(){
+// função que organiza os dados vindos da UACT CC e serializa com o timestamp e código da aplicação
+void OrganizePkg(){
   modbus.stillWaitNextBit = false;
   bool valid = modbus.validacaoPacote(uactComponent.packetReceived);
 
   if(valid){ // só entra neste bloco se o CRC do pacote de resposta da UACT CC estiver correto
     uactComponent.PkgTotal[0] = aplicacao;
+
+    // timestamp
     uactComponent.PkgTotal[1] = uactComponent.packetReceived[5];
     uactComponent.PkgTotal[2] = uactComponent.packetReceived[6];
     uactComponent.PkgTotal[3] = uactComponent.packetReceived[3];
@@ -119,6 +136,193 @@ void receivedPkgTimerCallback(){
   clearReceivedPackage();
 }
 
+void espera_min(unsigned long time_min) {
+  unsigned long time_ms = time_min * 60000UL; // converte minutos para milissegundos
+  unsigned long start_time = millis();
+  unsigned long end_time = start_time + time_ms;
+  
+  while (millis() < end_time) {
+    // aguarda até que o tempo final seja alcançado
+  }
+}
+
+
+
+void printarVars(){
+
+    // timestamp
+    unsigned long timestamp = ((unsigned long)uactComponent.PkgTotal[1] << 24) |  // Desloca o primeiro byte 24 bits para a esquerda
+                          ((unsigned long)uactComponent.PkgTotal[2] << 16) |  // Desloca o segundo byte 16 bits para a esquerda
+                          ((unsigned long)uactComponent.PkgTotal[3] << 8)  |  // Desloca o terceiro byte 8 bits para a esquerda
+                          ((unsigned long)uactComponent.PkgTotal[4]);        // Não precisa deslocar
+
+    setTime(timestamp);
+    
+    int dia = day();
+    int mes = month();
+    int ano = year();
+    int hora = hour();
+    int minuto = minute();
+
+
+    // variaveis
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[5] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[6] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[7] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[8];
+
+    TensaoBarramento = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[9] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[10] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[11] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[12];
+
+    Corrente1 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[13] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[14] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[15] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[16];
+
+    Corrente2 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[17] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[18] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[19] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[20];
+
+    Corrente3 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[21] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[22] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[23] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[24];
+
+    Corrente4 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[25] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[26] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[27] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[28];
+
+    Corrente5 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[29] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[30] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[31] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[32];
+
+    Corrente6 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[33] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[34] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[35] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[36];
+
+    Corrente7 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[37] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[38] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[39] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[40];
+
+    Corrente8 = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[41] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[42] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[43] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[44];
+
+    PotenciaCC = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[45] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[46] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[47] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[48];
+
+    EnergiaFornecida = conversor.valor_float;
+
+    conversor.valor_inteiro = ((uint32_t)uactComponent.PkgTotal[49] << 24) | 
+                              ((uint32_t)uactComponent.PkgTotal[50] << 16)|
+                              ((uint32_t)uactComponent.PkgTotal[51] << 8) | 
+                              (uint32_t)uactComponent.PkgTotal[52];
+
+    EnergiaConsumida = conversor.valor_float;
+
+    // Imprime a data e hora obtidas
+    if(dia<10){
+      Serial.print("0");
+      Serial.print(dia);
+    }
+    else{
+      Serial.print(dia);
+    }
+    Serial.print("/");
+    if(mes<10){
+      Serial.print("0");
+      Serial.print(mes);
+    }
+    else{
+      Serial.print(mes);
+    }
+    Serial.print("/");
+    Serial.println(ano);
+    if(hora<10){
+      Serial.print("0");
+      Serial.print(hora);
+    }
+    else{
+      Serial.print(hora);
+    }
+    Serial.print("h");
+    if(hora<10){
+      Serial.print("0");
+      Serial.print(minuto);
+    }
+    else{
+      Serial.print(minuto);
+    }
+    Serial.println("min");
+
+
+    Serial.print("Tensão:");
+    Serial.print(TensaoBarramento);
+    Serial.print(",");
+    Serial.print("Corrente_1:");
+    Serial.println(Corrente1);
+    Serial.print(",");
+    Serial.print("Corrente_2:");
+    Serial.println(Corrente2);
+    Serial.print(",");
+    Serial.print("Corrente_3:");
+    Serial.println(Corrente3);
+    Serial.print(",");
+    Serial.print("Corrente_4:");
+    Serial.println(Corrente4);
+    Serial.print(",");
+    Serial.print("Corrente_5:");
+    Serial.println(Corrente5);
+    Serial.print(",");
+    Serial.print("Corrente_6:");
+    Serial.println(Corrente6);
+    Serial.print(",");
+    Serial.print("Corrente_7:");
+    Serial.println(Corrente7);
+    Serial.print(",");
+    Serial.print("Corrente_8:");
+    Serial.println(Corrente8);
+    Serial.print(",");
+    Serial.print("Potencia:");
+    Serial.println(PotenciaCC);
+    Serial.print(",");
+    Serial.print("EnergiaFornecida:");
+    Serial.println(EnergiaFornecida);
+    Serial.print(",");
+    Serial.print("EnergiaConsumida:");
+    Serial.println(EnergiaConsumida);
+}
+
 // Função de envio do pacote por LoRa.
 void do_send(osjob_t* j) {
 
@@ -130,16 +334,19 @@ void do_send(osjob_t* j) {
     clearReceivedPackage();
     modbus.EnviarPacote(DeviceAdress,TypeRegisters,InitAdress,QuantRegisters); // envia a requisição
     espera(300); // aguarda 300 milissegundos
-    Serial.print("Pacote Recebido: ");
-    modbus.printar(uactComponent.packetReceived,sizeof(uactComponent.packetReceived)); // printa o pacote recebido da UACT CC
-    receivedPkgTimerCallback(); // organiza o pacote para transmissão LoRa
+    // Serial.print("Pacote Recebido: ");
+    // modbus.printar(uactComponent.packetReceived,sizeof(uactComponent.packetReceived)); // printa o pacote recebido da UACT CC
+    OrganizePkg(); // organiza o pacote para transmissão LoRa
     Serial.println("");
     Serial.print("Pacote para envio: ");
     modbus.printar(uactComponent.PkgTotal,sizeof(uactComponent.PkgTotal)); // printa o pacote organizado
-    
+
+    printarVars();
+
     LMIC_setTxData2(1, uactComponent.PkgTotal, sizeof(uactComponent.PkgTotal), 0); // envia o pacote organizado por LoRa
     Serial.println("Pacote enviado");
     clearReceivedPackageTotal(); // limpa o pacote de envio para não transmitir a informação anterior, quando houver erros no pacote
+    // espera_min(1);
   }
 }
 void onEvent (ev_t ev) {
