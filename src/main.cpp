b8bdd828cebda30b3486b71409748de251159c5e
joelalison <45297008+joelalison@users.noreply.github.com>
2024-05-03 12:21:16 -0300

    mudanca no codigo de aplicacao e refinamento do codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
-// Código de transmissão de dados da aplicação SISTEMA DE BATERIAS relacionada ao Smart Campus da UFPA
-// código editado em 03 de abril de 2023
+/*
+  Autor: Joel Carvalho - Mestre em Engenharia Elétrica pela Universidade Federal do Pará
+
+  Descrição: Este código tem por objetivo receber as informações do UACT CC por MODBUS e transmitir por LoRa
+  
+  Data de edição: 03/05/2024
+  
+  Versão: 2
+  
+  Notas adicionais: Protocolos envolvidos: MODBUS RTU/LoRa
+*/
 
 #include <Arduino.h>
 #include <lmic.h> // biblioteca lmic para transmissão LoRa
@@ -8,14 +17,14 @@
 #include <TimeLib.h> // biblioteca para converter timestamp no formato unix para hora e data
 #include <softwareReset.hpp> // biblioteca para resetar o microcontrolador
 
-MODBUS modbus(23,LED_BUILTIN);
+MODBUS modbus(23);
 
 // chaves de acesso AES128 da criptografia LoRaWAN
-static const PROGMEM u1_t NWKSKEY[16] = {/* Informação confidencial */}; //projeto
+static const PROGMEM u1_t NWKSKEY[16] = {/* Chave restrita*/};
 
-static const u1_t PROGMEM APPSKEY[16] = {/* Informação confidencial */}; //projeto
+static const u1_t PROGMEM APPSKEY[16] = {/* Chave restrita*/};
 
-static const u4_t DEVADDR = 0x260138A2 ; // projeto <-- Change this address for every node!
+static const u4_t DEVADDR = 0x26013804 ; // projeto <-- Change this address for every node!
 
 void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
@@ -32,7 +41,7 @@ const lmic_pinmap lmic_pins = {
   .dio = {2, 6, 7},
 };
 
-const byte aplicacao = 0x04; // código do sistema de baterias
+const byte aplicacao = 0x06; // código do sistema de baterias
 
 // parâmetros do protocolo MODBUS
 const byte DeviceAdress = 0x02; // endereço do dispositivo - UACT CC
@@ -40,43 +49,46 @@ const byte TypeRegisters = 0x04; // código de acesso dos registradores do tipo 
 const uint16_t InitAdress = 0x0004; // endereço do registrador inicial
 const uint16_t QuantRegisters = 0x001A; // quantidade de registradores
 
-float TensaoBarramento;
-float Corrente1;
-float Corrente2;
-float Corrente3;
-float Corrente4;
-float Corrente5;
-float Corrente6;
-float Corrente7;
-float Corrente8;
-float PotenciaCC;
-float EnergiaFornecida;
-float EnergiaConsumida;
 
+// Definir um vetor para armazenar os valores de deslocamento
+const int offsets[] = {5, 9, 13, 17, 21, 25, 29, 33, 37};
+
+
+const char *nomesVariaveis[] = {
+    "Tensão Barramento",
+    "Corrente 1",
+    "Corrente 2",
+    "Corrente 3",
+    "Corrente 4",
+    "Corrente 5",
+    "Corrente 6",
+    "Corrente 7",
+    "Corrente 8",
+    "Potência CC",
+    "Energia Fornecida",
+    "Energia Consumida"
+};
+
+union {
+   uint8_t timesVector8[4] ;      //  4 bytes
+   uint32_t times32 ;   //  mapped onto the same storage as myFloat
+} timestamp;
+
+// Quantidade de variáveis
+const int numVariaveis = sizeof(offsets) / sizeof(offsets[0]);
 
 // parâmetros dos dados recebidos da UAC CC
 struct UACTStruct
 {
-  byte packetReceived[57];
+  byte packetReceived[57]; // ID (dispositivo), codigoFuncao, QuantidadeBytesUteis, Variaveis, CRC
   byte receivedByteIndex = 0;
-  byte PkgTotal[53];
+  byte PkgTotal[numVariaveis*4+5]; // pacote de envio - ID (aplicacao), Timestamp, Variaveis
 };
 UACTStruct uactComponent;
 
 // função que é chamada sempre que existem dados na serial do arduino para serem lidos
 void serialEvent1(){
-  byte incomingByte;
-  // Serial.println(stillWaitNextBit);
-  if (modbus.stillWaitNextBit)
-  {
-    while (Serial1.available()) {
-      incomingByte = Serial1.read();
-      // Serial.print(incomingByte, HEX);
-      // Serial.print(' ');
-      uactComponent.packetReceived[uactComponent.receivedByteIndex] = incomingByte;
-      uactComponent.receivedByteIndex += 1;
-    }
-  }
+  if (modbus.stillWaitNextBit) while (Serial1.available()) Serial1.readBytes(uactComponent.packetReceived,57);
 }
 
 // função para controlar o tempo entre requisição e resposta à UACT CC
@@ -90,12 +102,67 @@ void espera(unsigned long tempo) {
 // função que limpa o payload de resposta da UACT CC
 void clearReceivedPackage(){
   for (byte i = 0; i < sizeof(uactComponent.packetReceived); i++) uactComponent.packetReceived[i] = 0x0;
-  uactComponent.receivedByteIndex = 0;
 }
 
 // função que limpa o payload de transmissão com todos os dados tratados
 void clearReceivedPackageTotal(){
   for (byte i = 0; i < sizeof(uactComponent.PkgTotal); i++) uactComponent.PkgTotal[i] = 0x0;
+}
+
+void printTimes(){
+  // timestamp
+  for(uint8_t i=1; i<5;i++){
+    timestamp.timesVector8[i-1] = uactComponent.PkgTotal[1];
+  }
+
+  setTime(timestamp.times32);
+  
+  int dia = day();
+  int mes = month();
+  int ano = year();
+  int hora = hour();
+  int minuto = minute();
+  int segundo = second();
+
+  // Imprime a data e hora obtidas
+    if(dia<10){
+      Serial.print("0");
+    }
+    Serial.print(dia);
+    Serial.print("/");
+
+    if(mes<10){
+      Serial.print("0");
+    }
+    Serial.print(mes);
+    Serial.print("/");
+    Serial.println(ano);
+
+    if(hora<10){
+      Serial.print("0");
+    }
+    Serial.print(hora);
+    Serial.print("h");
+
+    if(minuto<10){
+      Serial.print("0");
+    }
+    Serial.print(minuto);
+    Serial.println("min");
+
+    if(segundo<10){
+      Serial.print("0");
+    }
+    Serial.print(segundo);
+    Serial.println("min");
+}
+
+void printVarsFloat(){
+  for (int i = 0; i < numVariaveis; i++) {
+    Serial.print(nomesVariaveis[i]);
+    Serial.print(": ");
+    Serial.println(modbus.IEEE754_HexToFloat(uactComponent.PkgTotal, offsets[i])); 
+  }
 }
 
 // função que organiza os dados vindos da UACT CC e serializa com o timestamp e código da aplicação
@@ -111,6 +178,7 @@ void OrganizePkg(){
     uactComponent.PkgTotal[2] = uactComponent.packetReceived[6];
     uactComponent.PkgTotal[3] = uactComponent.packetReceived[3];
     uactComponent.PkgTotal[4] = uactComponent.packetReceived[4];
+
     // variaveis de interesse
     for (byte i = 5; i<41; i=i+4){
       // i=40
@@ -118,11 +186,6 @@ void OrganizePkg(){
       uactComponent.PkgTotal[i+1] = uactComponent.packetReceived[i+17]; // 22-5
       uactComponent.PkgTotal[i+2] = uactComponent.packetReceived[i+14]; // 19-5
       uactComponent.PkgTotal[i+3] = uactComponent.packetReceived[i+15]; // 20-5
-    }
-
-    // preenchendo com zero as variáveis com zeros
-    for (byte i=41; i<53; i++){
-      uactComponent.PkgTotal[i] = 0x00;
     }
   }
   else{ // se o pacote não estiver correto reseta o arduino
@@ -140,108 +203,6 @@ void espera_min(unsigned long time_min) {
   while (millis() < end_time) {
     // aguarda até que o tempo final seja alcançado
   }
-}
-
-void printarVars(){
-
-    // timestamp
-    unsigned long timestamp = ((unsigned long)uactComponent.PkgTotal[1] << 24) |  // Desloca o primeiro byte 24 bits para a esquerda
-                          ((unsigned long)uactComponent.PkgTotal[2] << 16) |  // Desloca o segundo byte 16 bits para a esquerda
-                          ((unsigned long)uactComponent.PkgTotal[3] << 8)  |  // Desloca o terceiro byte 8 bits para a esquerda
-                          ((unsigned long)uactComponent.PkgTotal[4]);        // Não precisa deslocar
-
-    setTime(timestamp);
-    
-    int dia = day();
-    int mes = month();
-    int ano = year();
-    int hora = hour();
-    int minuto = minute();
-
-    // variaveis
-    TensaoBarramento = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,5,6,7,8);
-    Corrente1 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,9,10,11,12);
-    Corrente2 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,13,14,15,16);
-    Corrente3 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,17,18,19,20);
-    Corrente4 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,21,22,23,24);
-    Corrente5 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,25,26,27,28);
-    Corrente6 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,29,30,31,32);
-    Corrente7 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,33,34,35,36);
-    Corrente8 = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,37,38,39,40);
-    PotenciaCC = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,41,42,43,44);
-    EnergiaFornecida = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,45,46,47,48);
-    EnergiaConsumida = modbus.IEEE754_HexToFloat(uactComponent.PkgTotal,49,50,51,52);
-
-    // Imprime a data e hora obtidas
-    if(dia<10){
-      Serial.print("0");
-      Serial.print(dia);
-    }
-    else{
-      Serial.print(dia);
-    }
-    Serial.print("/");
-    if(mes<10){
-      Serial.print("0");
-      Serial.print(mes);
-    }
-    else{
-      Serial.print(mes);
-    }
-    Serial.print("/");
-    Serial.println(ano);
-    if(hora<10){
-      Serial.print("0");
-      Serial.print(hora);
-    }
-    else{
-      Serial.print(hora);
-    }
-    Serial.print("h");
-    if(hora<10){
-      Serial.print("0");
-      Serial.print(minuto);
-    }
-    else{
-      Serial.print(minuto);
-    }
-    Serial.println("min");
-
-    Serial.print("Tensão:\t\t\t");
-    Serial.println(TensaoBarramento);
-
-    Serial.print("Corrente 1:\t\t");
-    Serial.println(Corrente1);
-
-    Serial.print("Corrente 2:\t\t");
-    Serial.println(Corrente2);
-
-    Serial.print("Corrente 3:\t\t");
-    Serial.println(Corrente3);
-
-    Serial.print("Corrente 4:\t\t");
-    Serial.println(Corrente4);
-
-    Serial.print("Corrente 5:\t\t");
-    Serial.println(Corrente5);
-
-    Serial.print("Corrente 6:\t\t");
-    Serial.println(Corrente6);
-
-    Serial.print("Corrente 7:\t\t");
-    Serial.println(Corrente7);
-
-    Serial.print("Corrente 8:\t\t");
-    Serial.println(Corrente8);
-
-    Serial.print("Potencia:\t\t");
-    Serial.println(PotenciaCC);
-
-    Serial.print("Energia Fornecida:\t");
-    Serial.println(EnergiaFornecida);
-
-    Serial.print("Energia Consumida:\t");
-    Serial.println(EnergiaConsumida);
 }
 
 // Função de envio do pacote por LoRa.
@@ -262,12 +223,14 @@ void do_send(osjob_t* j) {
     Serial.print("Pacote para envio: ");
     modbus.printar(uactComponent.PkgTotal,sizeof(uactComponent.PkgTotal)); // printa o pacote organizado
 
-    printarVars();
+    printTimes();
+    Serial.println();
+    printVarsFloat();
 
     LMIC_setTxData2(1, uactComponent.PkgTotal, sizeof(uactComponent.PkgTotal), 0); // envia o pacote organizado por LoRa
     Serial.println("Pacote enviado");
     clearReceivedPackageTotal(); // limpa o pacote de envio para não transmitir a informação anterior, quando houver erros no pacote
-    espera_min(4);
+    // espera_min(4);
   }
 }
 void onEvent (ev_t ev) {
@@ -356,6 +319,7 @@ void setup() {
 #else
 
   LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+  
 #endif
   LMIC_selectSubBand(1);
 
